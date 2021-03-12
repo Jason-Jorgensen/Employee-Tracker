4000b8f3b6f535224a5e9ea91653ac833bddd6ec
Jason Jorgensen <jasoncjorgensen@gmail.com>
2021-03-11 23:37:07 -0700

    added 2 view sql functions

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
-const cTable= require('console.table')
+const cTable = require('console.table')
+const roles = [];
+const departments = [];
 
 const connection = mysql.createConnection({
-    host:'localhost',
+    host: 'localhost',
     port: 3306,
     user: 'root',
-    password:'bootcamp21',
-    database:'employee_tracker'
+    password: 'bootcamp21',
+    database: 'employee_tracker'
 });
 
 connection.connect((err) => {
@@ -18,37 +20,114 @@ connection.connect((err) => {
     console.log(`connected as id ${connection.threadId}`);
 });
 
-const view = () => {
-    connection.query('select * from department', (err,departments) => {
-        console.table(departments);
+const update = () => {
+    roles.length=0;
+    departments.length=0;
+    connection.query('SELECT name FROM department', (err,res) =>{
+        if (err) throw err;
+        for(i=0; i <res.length; i++) {
+            departments.push(res[i].name)
+        }
+
+    connection.query('SELECT title FROM role', (err,res) => {
+        if (err) throw err;
+        for(i=0; i< res.length; i++) {
+            roles.push(res[i].title)
+        }
+    })
+    })
+}
+
+const viewJoin = () => {
+    connection.query(`
+    select employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary
+    from employee 
+    inner join role 
+    on employee.role_id = role.id
+    inner join department
+    on role.department_id = department.id`, (err, res) => {
+        console.table(res);
+        startTracker();
+    })
+};
+
+const viewEmpByRole = (response) => {
+    empRole = response.function;
+    connection.query(`
+    select employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary
+    from employee 
+    inner join role 
+    on employee.role_id = role.id
+    inner join department
+    on role.department_id = department.id
+    WHERE role.title = '${empRole}'`, (err, res) => {
+        console.table(res);
         startTracker();
     })
 };
 
 
-startTracker();
+// 'select employee.id, employee.first_name, employee_lastname, role.title, department.name, role.salary from employee ((inner join role on employee.role_id = role.id) inner join role on role.department_id = department.id);'
+
 const startTracker = () => {
+    update();
     inquirer.prompt([
-    {
-        type:'list',
-        name:'function',
-        message:'What would you like to do?',
-        choices: ['View All Eployees','View All Roles','View All Departments'],
-    },
-]).then((command) => {
-    let commands = command.function;
-    if (commands === 'View All Employees'){
-        console.log('yes')
-    }
-    else if(commands === 'View All Roles'){
-        console.log('no')
-    }
-    else if(commands === 'View All Departments'){
-        view();
-        
-    }
-})
+        {
+            type: 'list',
+            name: 'function',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees', 
+                'View Employees by Roles', 
+                'View Employees by Department'
+            ],
+        },
+    ]).then((response) => {
+        switch (response.function) {
+            case 'View All Employees':
+                viewJoin();
+                break;
+
+            case 'View Employees by Roles':
+                inquirer.prompt({
+                    type: 'list',
+                    name: 'function',
+                    message: 'Which Role?',
+                    choices: roles,
+                        }).then((response) =>{
+                            viewEmpByRole(response);
+                        })
+                break;
+
+            // case 'View Employees by Department':
+            //     // ();
+            //     break;
+
+            // case 'View All Employees':
+            //     // ();
+            //     break;
+            // case 'View All Employees':
+            //     // ();
+            //     break;
+
+            // case 'View All Employees':
+            //     // ();
+            //     break;
+
+            // case 'View All Employees':
+            //     // ();
+            //     break;
+
+            // case 'View All Employees':
+            //     // ();
+            //     break;
+        }
+
+    })
 };
+
+startTracker();
+
 
 
 
