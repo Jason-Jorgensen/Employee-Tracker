4a3d1699a913ec0173ac4e98686406830e6c6513
Jason Jorgensen <jasoncjorgensen@gmail.com>
2021-03-13 00:17:27 -0700

    Finished the view, add, and update functions of the SQL queries. Database loaded with Seed Data

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ connection.connect((err) => {
         console.error(`error connecting ${err.stack}`);
         return;
     }
-    console.log(`connected as id ${connection.threadId}`);
+    // console.log(`connected as id ${connection.threadId}`);
 });
 
 const update = () => {
@@ -66,6 +66,84 @@ const viewEmpByRole = (response) => {
     })
 };
 
+const viewEmpByDept = (response) => {
+    empDept = response.function;
+    connection.query(`
+    select employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary
+    from employee 
+    inner join role 
+    on employee.role_id = role.id
+    inner join department
+    on role.department_id = department.id
+    WHERE department.name = '${empDept}'`, (err, res) => {
+        console.table(res);
+        startTracker();
+    })
+};
+
+const addDepartment = (response) => {
+    let newDept = response.function;
+    connection.query(`
+    INSERT INTO department (name) VALUES ('${newDept}')
+    `);
+
+    connection.query(`
+    SELECT * from department`, (err, res) => {
+    console.table(res)});
+
+    startTracker();
+}
+
+const addRole = (response) => {
+    connection.query(`
+    SELECT id FROM department WHERE name = '${response.department_id}'
+    `
+     , (err, res) => {
+        if (err) throw err;
+        connection.query(`
+        INSERT INTO role (title, salary, department_id) VALUES ('${response.role}','${response.salary}','${res[0].id}')
+        `)
+        console.log(`Added ${response.role} as a new role.`)
+    })
+    update();
+
+    connection.query(`
+    SELECT * from role`, (err, res) => {
+    console.table(res)});
+
+    startTracker();
+}
+
+const addEmp = (response) => {
+    connection.query(`
+    SELECT id FROM role WHERE title = '${response.role}'
+    `, (err,res) => {
+        connection.query(`
+        INSERT INTO employee
+        (first_name, last_name, role_id) 
+        VALUES ('${response.firstname}','${response.lastname}','${res[0].id}')
+        `)
+    })
+    update();
+    viewJoin();
+    startTracker();
+
+}
+
+const updateEmp = (response) => {
+    connection.query(`
+    SELECT id FROM role WHERE '${response.newRole}' = title  
+    `, (err,res) => {
+        connection.query(`
+        UPDATE employee
+        SET role_id = '${res[0].id}'
+        WHERE '${response.who}' = employee.id
+        `)
+    })
+
+    viewJoin();
+}
+
 
 // 'select employee.id, employee.first_name, employee_lastname, role.title, department.name, role.salary from employee ((inner join role on employee.role_id = role.id) inner join role on role.department_id = department.id);'
 
@@ -79,7 +157,11 @@ const startTracker = () => {
             choices: [
                 'View All Employees', 
                 'View Employees by Roles', 
-                'View Employees by Department'
+                'View Employees by Department',
+                'Add a New Department',
+                'Add a New Role',
+                'Add Employee',
+                'Update Employee Role'
             ],
         },
     ]).then((response) => {
@@ -99,24 +181,106 @@ const startTracker = () => {
                         })
                 break;
 
-            // case 'View Employees by Department':
-            //     // ();
-            //     break;
+            case 'View Employees by Department':
+                inquirer.prompt({
+                    type: 'list',
+                    name: 'function',
+                    message: 'Which Role?',
+                    choices: departments,
+                        }).then((response) =>{
+                            viewEmpByDept(response);
+                        })
+                break;
 
-            // case 'View All Employees':
-            //     // ();
-            //     break;
-            // case 'View All Employees':
-            //     // ();
-            //     break;
+            case 'Add a New Department':
+                connection.query(`
+                SELECT * from department`, (err, res) => {
+                    console.table(res);
+                    inquirer.prompt({
+                        // type:'input',
+                        name:'function',
+                        message:'Type in the new Department name: '
+                    }).then((response) => {
+                        addDepartment(response);
+                    })
+                    })
+                break;
 
-            // case 'View All Employees':
-            //     // ();
-            //     break;
+            case 'Add a New Role':
+                connection.query(`
+                SELECT * from role`, (err, res) => {
+                    console.table(res);
+                    inquirer.prompt([
+                        {
+                        name:'role',
+                        message:'Type in the new Role title: '
+                        },
+                        {
+                        name:'salary',
+                        message:'What is the salary? ',
+                        },
+                        {
+                        type:"list",
+                        name:"department_id",
+                        message:"Which Department is the role associated with? ",
+                        choices: departments
+                        },]).then((response) => {
+                            addRole(response);
+                        })
+                        })
+                    break;
 
-            // case 'View All Employees':
-            //     // ();
-            //     break;
+            case 'Add Employee':
+                connection.query(`
+                SELECT * from employee`, (err, res) => {
+                    console.table(res);
+                    inquirer.prompt([
+                        {
+                        name:'firstname',
+                        message:"Type in Employee's First Name: "
+                        },
+                        {
+                        name:'lastname',
+                        message:"Type in Employee's Last Name: ",
+                        },
+                        {
+                        type:"list",
+                        name:"role",
+                        message:"Select Employee's Role: ",
+                        choices: roles
+                        }]).then((response) => {
+                            addEmp(response);
+                        })
+                        })
+
+                    break;
+
+            case 'Update Employee Role':
+                connection.query(`
+                SELECT employee.id, employee.first_name, employee.last_name, role.title
+                FROM employee
+                INNER JOIN role 
+                ON employee.role_id = role.id
+                `,(err,res)=> {
+                    console.table(res);
+                    inquirer.prompt([
+                        {
+                        name:'who',
+                        message:"Type the Employers id from the list above of who's role you would like to change:"
+                        },
+                        {
+                        type:'list',
+                        name:'newRole',
+                        message:"Select the new role you are assigning this person:",
+                        choices: roles
+                        }
+                    ]).then((response) => {
+                        updateEmp(response);
+                    })
+                })
+            
+
+                break;
 
             // case 'View All Employees':
             //     // ();
--- seed.sql
+++ seed.sql
@@ -1,9 +1,36 @@
 INSERT INTO department (name)
-VALUES ('Information Technology'),('Finance'),('Administration');
+VALUES 
+('Finance'),
+('Administration'),
+('Management'),
+('Sales'),
+('Intern'),
+('Customer Service')
 
 INSERT INTO role (title,salary,department_id)
-VALUES ('Manager',200000,1),('Engineer',115000,1),('CFO',250000,2);
+VALUES 
+('Manager',85000,3),
+('Sales Rep',80000,4)
+('Accountant',75000,1),
+('Temp',10000,5),
+('Receptionist',40000,2),
+('CFO',250000,1),
+('Quabity Assuance',50000,6),
+('Supplier Relations',50000,6),
+('Customer Service Rep',50000,6)
 
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Jason', 'Jorgensen',2),('Paige','Jorgensen',3);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES 
+('Michael', 'Scott',1),
+('Dwight','Schrute',2),
+('Kevin','Malone',3),
+('Ryan', 'Howard',4),
+('Pam','Halpart',5),
+('Jim','Halpart',2),
+('David','Wallace',6),
+('Oscar','Martinez',3),
+('Creed','Bratton',7),
+('Andy','Bernard',2);
+('Meredith','Palmer',8)
+('Kelly','Capor',9)
